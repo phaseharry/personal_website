8b92e30b23c579b72fac745c198d97221f1b0bca
Harry Chen <harrychenhc@icloud.com>
2018-12-10 21:55:59 -0500

    added photo

--- client/components/AboutMe.js
+++ client/components/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography, Avatar } from '@material-ui/core'
 
 class AboutMe extends React.Component{
   render(){
@@ -10,8 +10,19 @@ class AboutMe extends React.Component{
           I am a Fullstack Engineer, avid biker, and Esports enthusiast. 
           I love playing around with new technologies and APIs. 
           I am excited to immerse myself into the New York tech scene 
-          and looking forward to meeting great people.
+          and looking forward to meet great people.
         </Typography>
+        <div id='linksContainer'>
+          <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/harrychenhc/'>
+            <Avatar className='icon' src='/public/linked.png'/>
+          </a>
+          <a target='_blank' rel='noopener noreferrer' href='https://github.com/phaseharry'>
+            <Avatar className='icon' src='/public/git.png'/>
+          </a>
+          <a target='_blank' rel='noopener noreferrer' href='https://medium.com/@phaseharry'>
+            <Avatar className='icon' src='/public/medium-icon-21.jpg'/>
+          </a>
+        </div>
       </div>
     )
   }
--- client/components/SidePop.js
+++ client/components/SidePop.js
@@ -7,17 +7,9 @@ class SidePop extends React.Component{
   render(){
     return (
       <div id='aboutMeContainer'>
-        <AboutMe />
-        <div id='linksContainer'>
-          <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/harrychenhc/'>
-            <Avatar className='icon' src='/public/linked.png'/>
-          </a>
-          <a target='_blank' rel='noopener noreferrer' href='https://github.com/phaseharry'>
-            <Avatar className='icon' src='/public/git.png'/>
-          </a>
-          <a target='_blank' rel='noopener noreferrer' href='https://medium.com/@phaseharry'>
-            <Avatar className='icon' src='/public/medium-icon-21.jpg'/>
-          </a>
+        <div className='headshotContainer'>
+          <img id='headshot' src='/public/headshot.jpg'></img>
+          <AboutMe />
         </div>
       </div>
     )
