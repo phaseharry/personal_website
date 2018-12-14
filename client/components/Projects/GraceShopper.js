294a079d4054ecd5743822477761ca5fab75de5b
Harry Chen <harrychenhc@icloud.com>
2018-12-13 22:24:42 -0500

    updated once again

--- client/components/Projects/GraceShopper.js
+++ client/components/Projects/GraceShopper.js
@@ -6,9 +6,18 @@ class GraceShopper extends React.Component{
     return (
       <div className='individual'>
         <div className='projectText'>
-          <Typography align='center' variant='display1' className='heading'>Grace Shopper</Typography>
-          <Typography align='center' variant='body2' className='paragraphPro'>
-            What is GraceShopper? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <Typography align='center' variant='display1' className='heading'>Fullstacks of Trash</Typography>
+          <Typography align='center' variant='body2' className='paragraphPro' style={{paddingBottom: '20px'}}>
+            One man's trash is another man's treasure.
+          </Typography>
+          <Typography align='center' variant='body2' style={{paddingBottom: '20px'}}>
+            Fullstacks of Trash is an e-commerce platform made with the intention of selling TRASH. Anonymous and logged-in users can add trash to their carts,
+            remove if necessary, write reviews for products, and checkout their carts via payment.
+          </Typography>
+          <Typography align='center' variant='body2' className='paragraphPro'  style={{paddingBottom: '20px'}}>
+            The project was built by a team of 4. For my part, I touched both the backend as well as the frontend of the application. 
+            I set up our database models and relationships, designed RESTful API routes, wrote React components, and integrated Stripe for payment processing. 
+            Check it out  <a target='_blank' rel='noopener noreferrer' href='https://fullstacks-of-trash-final.herokuapp.com/'>here</a>!
           </Typography>
         </div>
         <div className='projectMedia'>
